139684853577da59924954d913354caeb5fa6ab1
Mário Ribeiro <mail@marioribeiro.com>
2017-06-06 17:47:26 +0100

    Add Unit test for Thermostat

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+describe("Thermostat", function(){
+  var thermostat;
+  var Thermostat = require('../lib/Thermostat');
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it("returns the starting temperature", function(){
+    expect(thermostat.temperature()).toEqual(20);
+  });
+});
